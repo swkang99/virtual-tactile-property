0195a5c3b54610238f45b7f8c630870bdd6a450b
swkang99 <swkang99@gm.gist.ac.kr>
2026-06-23 13:18:07 +0900

    Remove: data, experiments, result

--- src/model/factory.py
+++ src/model/factory.py
@@ -3,7 +3,7 @@ from src.model.prediction.compared.cnn_1d_scirep import CNN1DScirep
 from src.model.prediction.compared.ann import ANN
 from sklearn import linear_model
 from sklearn.svm import SVR
-from src.model.prediction.proposed.transformer import TransformerRegressor
+# from src.model.prediction.proposed.transformer import TransformerRegressor
 from src.model.prediction.proposed.cnn_1d_generic import CNN1DGeneric
 from src.model.prediction.proposed.gated_mlp import GatedFusionRegressor
 
@@ -13,7 +13,7 @@ MODEL_REGISTRY = {
     "ann": lambda conf, input_dim, device: ANN(conf, input_dim=input_dim).to(device),
     "cnn_1d_scirep": lambda conf, input_dim, device: CNN1DScirep(conf, input_dim=input_dim).to(device),
     "cnn_1d_4ha": lambda conf, input_dim, device: CNN1D4HA(conf, input_dim=input_dim).to(device),
-    "transformer": lambda conf, input_dim, device: TransformerRegressor().to(device),
+    # "transformer": lambda conf, input_dim, device: TransformerRegressor().to(device),
     "cnn_1d_generic": lambda conf, input_dim, device: CNN1DGeneric(input_dim=input_dim).to(device),
     "gated_mlp": lambda conf, input_dim, device: GatedFusionRegressor(input_dim=input_dim).to(device),
 }
